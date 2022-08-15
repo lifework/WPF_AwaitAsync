5381b920d11dc4ca7de02dbba7059e4774062818
lifework <kobayashi@lifework.tech>
2022-08-15 15:45:20 +0900

    呼び出し素の表示

--- WPF_AwaitAsync/MainWindow.xaml.cs
+++ WPF_AwaitAsync/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +28,29 @@ namespace WPF_AwaitAsync
 
         private void onRunButtonClicked(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"クリックされました");
+            AsyncVoidMethod();
+        }
+
+        private async void AsyncVoidMethod()
+        {
+            await Task.Run(() =>
+            {
+                WriteLine($"1");
+            });
+        }
+
+        private void WriteLine(string text)
+        {
+            Debug.WriteLine($"[{Caller()}][{DateTime.Now}] {text}");
+        }
+
+        private string Caller()
+        {
+            var caller = new System.Diagnostics.StackFrame(2, false);
+            var klass = caller.GetMethod()?.DeclaringType?.FullName;
+            var method = caller.GetMethod()?.Name;
+
+            return $"{klass}.{method}";
         }
     }
 }
